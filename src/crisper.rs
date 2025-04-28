9974aff274894fb0f7a21e351ff32fa2395ef423
Sean Shanahan <seshanahan66@gmail.com>
2025-04-28 12:35:30 -0400

    Practice with modules

---
+++ src/crisper.rs
@@ -0,0 +1,8 @@
+pub mod crisper;
+
+#[derive(Debug)]
+pub struct Crisper {
+    volume: i32,
+    occupied_volume: i32,
+    top_vegetable: String,
+}
---
+++ src/garden/garden.rs
@@ -0,0 +1,17 @@
+pub mod garden;
+
+#[derive(Debug)]
+pub struct Vegetable{
+    color: String,
+    family: String,    
+}
+
+impl Vegetable {
+    fn veg_color(&self) -> String {
+        color;
+    }
+
+    fn veg_family(&self) -> String {
+        family;
+    }
+} 
