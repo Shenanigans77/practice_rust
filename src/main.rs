927cf58e5244c249b81393fbe3722c005450904c
Sean Shanahan <seshanahan66@gmail.com>
2025-04-28 15:54:20 -0400

    Succesfully imported and ran crisper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+//use crate::garden::good_snails;
+mod crisper;
+use crisper::Crisper;
+
 fn main() {
     println!("Hello, world!");
+
+    let drawer = Crisper{
+        volume: 160,
+        occupied_volume: 0,
+    };
+
+    println!("{}", drawer.crisper_space())
 }
+ 
