e1f43a1916a7c9d91c33f14cca5726a87164c878
Sean Shanahan <seshanahan66@gmail.com>
2025-04-29 17:03:58 -0400

    Added 'name' to struct vegetable

--- src/kitchen_garden/vegetables.rs
+++ src/kitchen_garden/vegetables.rs
@@ -1,16 +1,17 @@
 
 #[derive(Debug)]
 pub struct Vegetable{
+    pub name: String,
     pub color: String,
     pub family: String,    
 }
 
 impl Vegetable {
-    pub fn veg_color(&self) -> String {
-        color
+    pub fn veg_color(&self) -> &String {
+        &self.color
     }
 
-    pub fn veg_family(&self) -> String {
-        family
+    pub fn veg_family(&self) -> &String {
+        &self.family
     }
 } 
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
+use crate::kitchen_garden::vegetables::Vegetable;
+//pub use crate::garden::good_snails;
 
-//use crate::garden::good_snails;
+mod kitchen_garden;
 mod crisper;
-use crisper::Crisper;
 
 fn main() {
     println!("Hello, world!");
 
-    let drawer = Crisper{
+    let drawer = crisper::Crisper{
         volume: 160,
         occupied_volume: 0,
     };
 
-    println!("{}", drawer.crisper_space())
+    println!("{}", drawer.crisper_space());
+
+    let squash = Vegetable{
+        name: String::from("Squash"),
+        color: String::from("Yellow"),
+        family: String::from("Gourd"),
+    };
+
+    println!("{}: a {} colored vegetable of the {} family.", squash.name, squash.color, squash.family)
 }
  
